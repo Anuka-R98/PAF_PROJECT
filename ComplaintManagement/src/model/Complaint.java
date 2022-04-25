2800d758869756808c5c6cf2dff54e78e079f948
SasinduMallawarachchi <it20200206@my.sliit.lk>
2022-04-25 05:43:25 +0530

    Complaint Management System

---
+++ ComplaintManagement/src/model/Complaint.java
@@ -0,0 +1,156 @@
+package model;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Complaint {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");// this sample 1
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/electri?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertComplaint(String customerName, String customerPNO, String description) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into complaint(`cID`,`customerName`,`customerPNO`,`description`)"
+					+ " values (?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, customerName);
+			preparedStmt.setString(3, customerPNO);
+			preparedStmt.setString(4, description);
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the complaint.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String readComplaint() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>ID</th><th>Customer Name</th><th>Customer PhoneNumber</th><th>Description</th>";
+			String query = "select * from complaint";
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String cID = Integer.toString(rs.getInt("cID"));
+				String customerName = rs.getString("customerName");
+				String customerPNO = rs.getString("customerPNO");
+				String description = rs.getString("description");
+				
+				// Add into the html table
+				output += "<tr><td>" + cID + "</td>";
+				output += "<td>" + customerName + "</td>";
+				output += "<td>" + customerPNO + "</td>";
+				output += "<td>" + description + "</td>";
+				
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the complaint.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String updateComplaint(String cID, String customerName, String customerPNO, String description) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+
+			// create a prepared statement
+			String query = "UPDATE complaint SET customerName=?,customerPNO=?,description=?" + "WHERE cID=?";
+		
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setString(1, customerName);
+			preparedStmt.setString(2, customerPNO);
+			preparedStmt.setString(3, description);
+			preparedStmt.setInt(4, Integer.parseInt(cID));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Updated successfully";
+		} catch (Exception e) {
+			output = "Error while updating the customer.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String deleteComplaint(String cID) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+
+			// create a prepared statement
+			String query = "delete from complaint where cID=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(cID));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the complaint.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+}
